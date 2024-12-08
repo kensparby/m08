e5ec19698e4c024e3574c75a9772e6622e8429aa
Ken Sparby <sparby.k@gmail.com>
2024-12-08 16:25:27 +0100

    TIME FOR A BREAK I THINK

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "web_link" {
   value = azurerm_storage_account.sa.primary_web_endpoint
 }
 
-output "primary_web_output" {
+output "primary_web_endpoint" {
   value = azurerm_storage_account.sa.primary_web_endpoint
 }
