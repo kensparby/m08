682da0f3d2cb8cf62d140f71736ce70481e4b659
Ken Sparby <sparby.k@gmail.com>
2024-12-08 13:29:36 +0100

    Change to index.html, as a test

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "azurerm_storage_blob" "index_html" {
 
 resource "local_file" "index_html" {
   filename = var.index_document
-  content  = "<h1>Hello World</h1>"
+  content  = "<h1>Hello World</h1><br><p>This is a test</p>"
 }
 
 resource "azurerm_storage_account_static_website" "sa_web" {
